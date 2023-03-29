0015e9b8e0414a9872e9910d364861f85e3c3c56
AnkitK <cosmicyokai100@gmail.com>
2023-03-29 21:05:59 +0530

    Add Header Brand and WhatGPT3-semiComplete.

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { Feature } from "../../components";
 import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      <h1>WhatGPT3</h1>
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          Possibilities are beyond your imagination
+        </h1>
+        <p>Explore the library</p>
+      </div>
+
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   );
 };
-
 export default WhatGPT3;
